c8c340cb730af2ac46370e1c1ecadb5a5580e5d0
ritika-neema <ritikaneema@gmail.com>
2018-11-18 23:42:43 +0530

    Initial commit

---
+++ app/src/main/java/com/ritikaneema/newsappstage2/NewsAdapter.java
@@ -0,0 +1,50 @@
+package com.ritikaneema.newsappstage2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class NewsAdapter extends ArrayAdapter<News> {
+
+    public NewsAdapter(Context context, ArrayList<News> news) {
+        super(context, 0, news);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.news_list_item, parent, false);
+        }
+
+        News currentNews = getItem(position);
+        TextView newsTitleTextView = (TextView) listItemView.findViewById(R.id.title_text_view);
+        String title = currentNews.getmTitle();
+        newsTitleTextView.setText(title);
+
+        TextView newsCategorytextView = (TextView) listItemView.findViewById(R.id.category_text_view);
+        String category = currentNews.getmCategory();
+        newsCategorytextView.setText(category);
+
+        TextView newsDatetextView = (TextView) listItemView.findViewById(R.id.date_text_view);
+        String date = currentNews.getmDate();
+        newsDatetextView.setText(date);
+
+        TextView newsAuthortextView = (TextView) listItemView.findViewById(R.id.author_text_view);
+        String author = currentNews.getmAuthor();
+
+        if(author.equals("")){
+            author = getContext().getString(R.string.noauthor);
+        }
+
+        newsAuthortextView.setText(author);
+
+        return listItemView;
+    }
+}
---
+++ app/src/main/java/com/ritikaneema/newsappstage2/NewsLoader.java
@@ -0,0 +1,31 @@
+package com.ritikaneema.newsappstage2;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+public class NewsLoader extends AsyncTaskLoader<List<News>> {
+
+    String mURL ;
+
+    public NewsLoader(Context context, String url) {
+        super(context);
+        this.mURL = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public List<News> loadInBackground() {
+        if (mURL == null) {
+            return null;
+        }
+
+        List<News> newsList = QueryUtils.fetchNewsData(mURL);
+        return newsList;
+    }
+}
